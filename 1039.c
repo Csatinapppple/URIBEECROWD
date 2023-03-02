ad3e98c73c2de6cc1f10b89bea1e683d9bef1627
Csatinapppple <88691457+Csatinapppple@users.noreply.github.com>
2023-03-02 09:55:38 -0300

    Update 1039.c

--- 1039.c
+++ 1039.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
-#define sumAndCompare(x,y,z,a,b,c) (x>=a && x+y+z>=a+b+c) ? true : false
+#define sumAndCompare(x,y) (x<=y) ? true : false
 
 //circle one encompasses circle two return true
 
 int main(){
   int rad1,x1,y1; //hunter
   int rad2,x2,y2; //flower
+  
   while(scanf("%d %d %d %d %d %d"
-        ,&rad1,&x1,&y1,&rad2,&x2,&y2) != EOF){
+  ,&rad1,&x1,&y1,&rad2,&x2,&y2) != EOF){
+      
+    float distance_squared = sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)));
 
-    bool print = sumAndCompare(rad1,x1,y1,rad2,x2,y2);
+    bool print = sumAndCompare(distance_squared+rad2,rad1);
     printf(print ? "RICO\n" : "MORTO\n");
   }
+
   return EXIT_SUCCESS;
 }
