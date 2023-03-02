53caf3301bedd1c251270aca32a03f7e4efeac0f
Csatinapppple <88691457+Csatinapppple@users.noreply.github.com>
2023-03-01 23:13:18 -0300

    Add files via upload

---
+++ 1039.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define 
+
+//circle one encompasses circle two return true
+
+typedef struct{
+  int radius,x,y;
+}Circle;
+
+int main(void){
+  Circle flower;
+  Circle hunter;
+  while(scanf("%d %d %d %d %d %d",&hunter.radius,&hunter.x,&hunter.y,
+    &flower.radius,&flower.x,&flower.y) != EOF){
+    
+  }
+
+  return EXIT_SUCCESS;
+}
