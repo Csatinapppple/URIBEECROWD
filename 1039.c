2c7b65eca0dcbdd0a7419d407421e4935a9ef5e8
Csatinapppple <88691457+Csatinapppple@users.noreply.github.com>
2023-03-02 01:49:35 -0300

    Update 1039.c

--- 1039.c
+++ 1039.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#define 
+#define sumAndCompare(x,y,z,a,b,c) (x>=a && x+y+z>=a+b+c) ? true : false
 
 //circle one encompasses circle two return true
 
-typedef struct{
-  int radius,x,y;
-}Circle;
+int main(){
+  int rad1,x1,y1; //hunter
+  int rad2,x2,y2; //flower
+  while(scanf("%d %d %d %d %d %d"
+        ,&rad1,&x1,&y1,&rad2,&x2,&y2) != EOF){
 
-int main(void){
-  Circle flower;
-  Circle hunter;
-  while(scanf("%d %d %d %d %d %d",&hunter.radius,&hunter.x,&hunter.y,
-    &flower.radius,&flower.x,&flower.y) != EOF){
-    
+    bool print = sumAndCompare(rad1,x1,y1,rad2,x2,y2);
+    printf(print ? "RICO\n" : "MORTO\n");
   }
-
   return EXIT_SUCCESS;
 }
