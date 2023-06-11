6983f689cf30780046b95a32bd1b0a4f8abd5f04
Csatinapppple <88691457+Csatinapppple@users.noreply.github.com>
2023-06-10 23:26:50 -0300

    Add files via upload

---
+++ 1053.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+double marca_e_mede(vector<tuple<int,int>> pontos){
+  double ret = 0;
+  int tab[5][5];
+  for(int i = 0;i<pontos.size();i++){
+      
+    
+
+  }
+
+  return ret;
+}
+
+int main(){
+  
+  vector<tuple<int,int>> pontos_marcados;
+  
+  
+
+  return 0;
+}
---
+++ 1053.hs
@@ -0,0 +1,14 @@
+-- case 1         case 2
+--   1 2 3 4 5   1 2 3 4 5
+-- a x . . . . a x . . . x
+-- b . . . . . b . . . . .
+-- c . x x x . c . . . . .
+-- d . x . . . d . . . . .
+-- e . . . . . e x . . . x
+
+--                     x1     x2     y1     y2
+calculateTrajectory :: Int -> Int -> Int -> Int -> 
+
+main :: IO()
+main = do
+  
